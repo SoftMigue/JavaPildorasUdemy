a2219709704fa1dbb2ceb5623e854824c0913980
migue <cumplidomiguelangel@gmail.com>
2021-07-12 21:05:17 +0200

    Comienzo del proyecto

---
+++ src/Pruebas.java
@@ -0,0 +1,24 @@
+
+public class Pruebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double FACTORCONVERSION = 1.3;
+		System.out.println(FACTORCONVERSION);
+		
+		int num1, num2, resultado, division;
+		 num1=5;
+		 num2=60;
+		 resultado=num1+num2;
+		 resultado+=10;
+		 division=num2/num1;
+		 System.out.println(resultado);
+		 System.out.println(division);
+		
+	}
+	
+	
+	
+
+}
+ 
